69e4dfed646e39eeb4245bb95990bd267bdb5ef8
Thenmozhi123 <40298467+Thenmozhi123@users.noreply.github.com>
2018-10-05 20:43:06 +0530

    Array programs
    how to find odd and even numbers from the given array={1,2,3,4,5,6,7,8}
    and we are expect output 1,3,5,7,2,4,6,8

---
+++ work_program.java
@@ -0,0 +1,27 @@
+package work;
+
+public class work_program {
+              
+	public static void main(String[] args) {
+		// given array 
+		//to find output: 1357 2468
+			int a[]={1,2,3,4,5,6,7,8};
+			for(int i=0;i<a.length;i++)
+			   {
+			     if(a[i]%2!=0)
+			    {
+				   System.out.print(a[i]+",");
+				 }
+			   }
+			for(int i=0;i<a.length;i++)
+			  {	
+			    if(a[i]%2==0)
+				  {
+				 System.out.print(a[i]+",");
+				  }
+			  }
+	}
+			}
+			
+	
+	
